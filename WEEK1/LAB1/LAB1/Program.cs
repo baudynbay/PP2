e84e8d477ce6a281b9939f43f24022710e3dabeb
Baudynbay <baudynbay1@gmail.com>
2019-01-26 11:57:03 +0600

    week 1 lab1

---
+++ WEEK1/LAB1/LAB1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+                string[] arr = Console.ReadLine().Split();
+
+            foreach(string s in arr)
+            {
+                if (int.Parse(s) % 2 == 0)
+                    Console.Write(s);
+                Console.Write(" ");
+            }
+
+
+        }
+    }
+}
