698d4651731b2a3b3696cfee0f51382d7c1eb706
Baudynbay <baudynbay1@gmail.com>
2019-01-29 02:50:07 +0600

    added files

--- WEEK1/LAB1/LAB1/Program.cs
+++ WEEK1/LAB1/LAB1/Program.cs
@@ -1,28 +1,35 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace LAB1
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            List<string> list = new List<string>();
             int n = int.Parse(Console.ReadLine());
             string[] arr = Console.ReadLine().Split();
 
             foreach (string s in arr)
             {
                 int sum = 0;
+
                 for (int i = 2; i <= arr.Length; i++) {
                     if (int.Parse(s) % i == 0)
+                    {
+                      
                         sum += 1;
+                    }
                 }
                 if (sum == 1) {
-                    Console.Write(s);
-                    Console.Write(" ");
+                    list.Add(s);
                 } }
 
-
+            Console.WriteLine(list.Count);
+            foreach(string s in list)
+            {
+                Console.Write(s + " ");
+            }
         }
     }
 }
