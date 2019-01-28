13bd41126a223b11d7dd9e3783dd1a551c116505
Baudynbay <baudynbay1@gmail.com>
2019-01-29 02:56:25 +0600

    added comments

--- WEEK1/LAB1/LAB1/Program.cs
+++ WEEK1/LAB1/LAB1/Program.cs
@@ -1,34 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic; //чтобы мы смогли использовать динамический массив
 namespace LAB1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> list = new List<string>();
-            int n = int.Parse(Console.ReadLine());
-            string[] arr = Console.ReadLine().Split();
+            List<string> list = new List<string>(); //создаем динамический массив 
+            int n = int.Parse(Console.ReadLine()); // читаем сколько чисел надо ввести 
+            string[] arr = Console.ReadLine().Split(); //заполняем массив
 
-            foreach (string s in arr)
+            foreach (string s in arr) //для каждого элемента этого массива 
             {
                 int sum = 0;
 
                 for (int i = 2; i <= arr.Length; i++) {
-                    if (int.Parse(s) % i == 0)
+                    if (int.Parse(s) % i == 0) // проверяем делится ли без остатка 
                     {
                       
-                        sum += 1;
+                        sum += 1; // если да то придавляем один
                     }
                 }
                 if (sum == 1) {
-                    list.Add(s);
+                    list.Add(s); // если число простое то добавляем в динамический массив
                 } }
 
-            Console.WriteLine(list.Count);
+            Console.WriteLine(list.Count); //выводим сколько чисел есть в массиве
             foreach(string s in list)
             {
-                Console.Write(s + " ");
+                Console.Write(s + " "); // выводим
             }
         }
     }
