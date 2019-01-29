83b456bb4dffb8f8863719136c53adcbc9f97b97
Baudynbay <baudynbay1@gmail.com>
2019-01-29 11:44:27 +0600

    task 3

---
+++ WEEK1/LAB1/task2/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task2
+{ 
+
+class Student
+{
+    public string name;
+    public string id;
+    public int yearOfStudy;
+
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+         }
+    }
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
