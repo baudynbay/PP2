b29cb4222acc9c4b2fa09f6231541963600dccd2
Baudynbay <baudynbay1@gmail.com>
2019-01-29 11:49:51 +0600

    added task3

---
+++ WEEK1/LAB1/task3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] arr = Console.ReadLine().Split();
+            List<string> number = new List<string>();
+
+
+            foreach(string s in arr)
+            {
+                number.Add(s);
+                number.Add(s);
+            }
+
+            foreach(string s in number)
+            {
+                Console.Write(s + ' ');
+            }
+        }
+    }
+}
