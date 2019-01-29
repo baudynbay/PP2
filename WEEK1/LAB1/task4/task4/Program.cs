abfed8cd55677f5391d4c1a6f7bb0e4b0d68fbc4
Baudynbay <baudynbay1@gmail.com>
2019-01-30 02:04:21 +0600

    added task 4

---
+++ WEEK1/LAB1/task4/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           
+                for (int  j = 1 ; j<= n; j++)
+                {
+                    for (int i =1; i<=j; i++)
+                {
+                    Console.Write("[*]");
+                }
+                Console.WriteLine();
+            }
+            
+
+                                        
+        }
+    }
+}
