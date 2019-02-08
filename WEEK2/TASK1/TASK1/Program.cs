d5460601fb40b8010de190cf31d24b7a0c57faa4
Baudynbay <baudynbay1@gmail.com>
2019-02-09 00:45:38 +0600

    lab2

---
+++ WEEK2/TASK1/TASK1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace TASK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader a = new StreamReader("a.txt");
+            string n = a.ReadToEnd();
+            char[] text = n.ToCharArray();
+            Array.Reverse(text);
+            string newText = new string(text);
+
+            if (newText == n)
+            {
+                Console.Write("YES");
+            }
+            else
+            {
+                Console.Write("NO");
+            }
+            a.Close();
+        }
+    }
+}
